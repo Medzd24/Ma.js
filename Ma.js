07a5df9f2513492388f79d31bd9a1a0a25885945
Medzd24 <150199255+Medzd24@users.noreply.github.com>
2023-11-15 22:49:01 +0100

    Create Ma.js

---
+++ Ma.js
@@ -0,0 +1,31 @@
+const bloggerId = '8781535279728903487';
+        const apiKey = 'AIzaSyA1fYK3qFyMuAMmm_LjaGBRf5-RD5Nfhvg';
+        const apiUrl = `https://www.googleapis.com/blogger/v3/blogs/${bloggerId}/posts?key=${apiKey}&maxResults=4`;
+
+        const postLinks = []; // مصفوفة لتخزين روابط المقالات
+
+        async function fetchHeadlines() {
+            try {
+                const response = await fetch(apiUrl);
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+
+                const data = await response.json();
+
+                // تحديث العناوين والروابط
+                for (let i = 0; i < 4; i++) {
+                    const headlineElement = document.getElementById(`headline${i + 1}`);
+                    
+                    headlineElement.innerText = data.items[i].title;
+                    headlineElement.href = data.items[i].url;
+                    
+                    postLinks[i] = data.items[i].url; // تخزين الرابط في المصفوفة
+                }
+            } catch (error) {
+                console.error('حدث خطأ أثناء جلب العناوين:', error);
+            }
+        }
+
+        fetchHeadlines();
